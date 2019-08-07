33a3d1911580f2b03adf138f23a02c65948e96a4
cabrennan <christine@cabrennan.com>
2019-08-06 21:52:59 -0400

    Initial deal function working

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    public Card card;
+    // Start is called before the first frame update
+    //public Sprite face = Card.face;
+  // public icon = Card.face;
+
+}
--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -3,94 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
 public class Deck : MonoBehaviour
 {
-    #region Singleton
+    public Vector3 thisPos = new Vector3(-2.93f, 2.43f, 1.28f);
+    public GameObject obj;
 
-    // items list will need Sprite association for game image
-    // In heiarchy attach approriate collider to sprite
-    // Attach interactibile script to item in heiarchy
-    // Attach itempickup script to item in heiarchy
-    // In inspector insert Inventory/items/item to variable in ItemPickup(script) dialog box. 
-
-    public static Deck instance;
-    public List<Card> deck; 
-    //public Queue<Card> cardQueue;
-
-    public Vector3[] positions = new Vector3[4];
-
-   // positions[0] = new Vector3(-2.93f,2.43f, 1.28f);
-   // positions[1] = new Vector3(1.62f, 2.5f, 1.35f);
-   // positions[2] = new Vector3(0.03f, -0.82f, -1.95f);
-   // positons[3] = new Vector3(-1.17f, 2.45f, 1.3f);
-    
     void Awake()
     {
-        instance = this;
-        //cardQueue = new Queue<Card>();
-        deck =  new List<Card>();
-        ShuffleDeck();
-        Debug.Log("after ShuffleDeck");
-        Deal();
+        var deck = GetDeck();
+        Debug.Log("after GetDeck");
+        var positions = GetPositions();
+        Debug.Log("after GetPostions");
+        Deal(deck, positions);
     }
 
-    #endregion
-
-    //public delegate void OnItemChanged();
-    //public OnItemChanged onItemChangedCallback;
-
-    // public int space = 15; // number of inventory slots 
-    // public Vector3 position;
-
-    //private void Start()
-    //{
-    //}
-
-
-
-
-    public void Add(Card c)
+    public Queue GetPositions()
     {
-        deck.Add(c);
-   //     if (onItemChangedCallback != null)
-   //     {
-   //         onItemChangedCallback.Invoke();
-   //     }
-   
-   }
-
-    public void Remove(Card c)
-    {
-        deck.Remove(c);
+        Queue pos = new Queue();
+            
+        pos.Enqueue((-2.93f, 2.43f, 1.28f));
+        pos.Enqueue((-1.17f, 2.45f, 1.3f));
+        pos.Enqueue((1.62f, 2.5f, 1.35f));
+        pos.Enqueue((3.42f, 02.54f, 1.39f));
+        return pos;
     }
 
-
-    public void ShuffleDeck()
+    public List<Card> GetDeck()
     {
-        Debug.Log("Inside ShuffleDeck");
-        string[] cardGuids = AssetDatabase.FindAssets("t:Card");
-        foreach (string s in cardGuids)
+        List<Card> deck = new List<Card>();
+        string[] itemGuids = AssetDatabase.FindAssets("t:Card");
+        foreach (string s in itemGuids)
         {
             string path = AssetDatabase.GUIDToAssetPath(s);
             Card c = AssetDatabase.LoadAssetAtPath<Card>(path);
-                this.Add(c);
             deck.Add(c);
-            Debug.Log("Added card: " + c.name);
         }
+        return deck;
+
     }
 
-    void Deal()
+    public void Deal(List<Card> deck, Queue pos)
     {
-        int i = 0;
+        float[,] positions = new float[,]
+        {
+            { -2.93f, 2.43f, 1.28f },
+            {-1.17f, 2.45f, 1.3f },
+            { 1.62f, 2.5f, 1.35f },
+            { 3.42f, 02.54f, 1.39f }
+        };
+
+        var idx = 0;
+
         foreach (Card c in deck)
         {
-            Debug.Log("Working on " + c.name);
-            Instantiate(c, new Vector3(-2.93f, 2.43f, 1.23f), Quaternion.identity);
             
-            // newcard.name = c.name;
-            //c.  = positions[i];
-            i++;
-        }
-    }
+            //(Vector3)pos.Dequeue();
+            Debug.Log("position is: " + pos);
+            Debug.Log("card is: " + c.name);
 
+
+            
+
+            // Vector3[] positionArray = new[] { new Vector3(0f, 0f, 0f), new Vector3(1f, 1f, 1f) };
+
+            obj = GameObject.Find(c.name);
+            var stuff  = pos.Dequeue();
+
+            Debug.Log("Stuff: " + stuff.ToString());
+
+            //thisPos = StringToVector3(stuff.ToString);
+
+            Vector3 point = new Vector3(positions[idx,0], positions[idx,1], positions[idx,2]);
+
+
+            Instantiate(obj, point, Quaternion.identity);
+            idx++;
+        }
+
+
+    }
 }
+
+
