2d551a855b70de7a5b6fec2de58135858d103178
cabrennan <christine@cabrennan.com>
2019-08-07 19:46:06 -0400

    whoops

--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class CardView : MonoBehaviour
 {
     public Card card;
+
+    public void Start()
+    {
+        card.enabled = false;
+    }
     // Start is called before the first frame update
     //public Sprite face = Card.face;
-  // public icon = Card.face;
+    // public icon = Card.face;
 
 }
